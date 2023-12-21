09c070cdce8ab129bcacf4a4a9ac775b751a4d56
RPDIT <83042903+RPDIT@users.noreply.github.com>
2023-12-21 10:48:10 -0500

    Properly branched the repo hoping I will be able to push this commit.

--- player.py
+++ player.py
@@ -2,55 +2,26 @@ class Player ():
    
     def __init__( self, marker, name): 
         self.marker = marker
+        self.display_board = [["","",""],["","",""],["","",""]]
         self.name = name
-   
-    def takeTurn(self, inputBoard):
-        while True:
-            move1 = self.getInput()
-            if self.validateInput(move1):
-                if self.checkPosition(move1, inputBoard):
-                    inputBoard = self.placeMarker(move1, inputBoard)
-                    break
-        self.showBoard(inputBoard)
-        return self.checkWin(inputBoard)
     
-    def getInput(self):
-        while True:
-                cords = input(f" Player {self.marker}: Enter the x,y values ")
-                cordStr = str(cords)
-                try: 
-                    cordArray = cordStr.split(" , ")
-                    cords = [int(d) for d in cordArray[0].split(',')]
-                    return cords
-                except:
-                    print("Please re-enter your input with only an X and Y value separated by a comma within the ranges 0-2. IE: 0,1 0,0 2,2")
-                    continue
-    
-    def validateInput(self, move):
-        for x in move: 
-            if x > 2:
-                print("Your input is larger than the 0-2 range of the board")
-                return False
-            if 0 > x: 
-                print("Your input must be positive")
-                return False
-        return True
-    
+    def takeTurn(self, inputBoard, move1): # returns 0 or 1 value to check if the current player has won
+        if self.checkPosition(move1, inputBoard):
+            return self.placeMarker(move1, inputBoard) 
+        else:
+            return 0
+
     def checkPosition(self, cords, board):
         if board[cords[0]][cords[1]] != "":
             print("That position is taken")
             return False
         else: 
-            return True 
-    
-    def placeMarker(self, cords, board): # takes x,y values as a list of ints as cords  
+            return True  
+   
+    def placeMarker(self, cords, board): # takes x,y values as a list of ints as cords
         board[cords[0]][cords[1]] = self.marker
-        return board
-
-    def showBoard(self, displayBoard):
-        print(f" {displayBoard[2][0]}  |  {displayBoard[2][1]}  |  {displayBoard[2][2]} ")
-        print(f" {displayBoard[1][0]}  |  {displayBoard[1][1]}  |  {displayBoard[1][2]} ")
-        print(f" {displayBoard[0][0]}  |  {displayBoard[0][1]}  |  {displayBoard[0][2]} ")
+        self.board = board
+        return self.checkWin(board)
     
     def checkWin(self, boardInput):
         matrix = self.genMatrix(boardInput)
@@ -62,7 +33,7 @@ class Player ():
             return 2
         else:
             return 0
-
+        
     def genMatrix(self, inputBoard):
         return [
             inputBoard[0],
@@ -74,17 +45,13 @@ class Player ():
             [inputBoard[0][0], inputBoard[1][1], inputBoard[2][2]],
             [inputBoard[0][2], inputBoard[1][1], inputBoard[2][0]] 
         ]
-
+    
     def checkStalemate(self, boardInput):
         for row in boardInput:
             for entry in row:
                 if entry == "":
                     return False
         return True
-    
-    
-
-
 
 
 
